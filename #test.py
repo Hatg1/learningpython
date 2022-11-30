372361ba553df97ae8f5f8d98b3bac5287520b64
nick <hatg1email@gmail.com>
2022-11-30 16:20:03 +1100

    30/11/22

--- #test.py
+++ #test.py
@@ -7,5 +7,16 @@ if 5 > 2:
 print("test")
 
 
-name = input("what is your name?\n")
-print("hello " + name)
+#name = input("what is your name?\n")
+#print("hello " + name)
+
+age = 24
+print(type(age))
+
+if type(age) == int and 1 < 2:
+    print(age)
+else:
+    print("whoops")
+    exit()
+
+# if statement
